c77b1f67ae45d6ffc37ea1d896d1415d0ac8d78b
JvE <joris.vaneijden@oneshoe.nl>
2015-11-29 14:51:00 +0100

    Start admin pages. Begin with users.

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -1,6 +1,7 @@
 <?php namespace Stellar\Http\Controllers\Admin;
 
 use Response;
+use Stellar\User;
 
 class UserController extends AdminController
 {
@@ -12,7 +13,18 @@ class UserController extends AdminController
      */
     public function index()
     {
-
+        $grid = \DataGrid::source(User::with('faction'))->attributes(array('class' => 'table table-hover table-striped'));
+        $grid->add('id', 'ID', true);
+        $grid->add('status', 'Status', true);
+        $grid->add('name', 'Name', true);
+        $grid->add('faction.name', 'Faction', true);
+        $grid->add('reputation', 'Reputation', true);
+        $grid->add('alignment', 'Alignment', true);
+        $grid->add('affiliation', 'Affiliation', true);
+        $grid->add('created_at', 'Created', true);
+        $grid->add('updated_at', 'Last change', true);
+        $grid->add('email', 'Email address');
+        return view('user.index', compact('grid'));
     }
 
 
